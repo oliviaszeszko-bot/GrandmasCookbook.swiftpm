9bd5d4926537c9b9e6f70713b4800269a03df3c5
Zahra Babayeva <zbabayeva9026@stu.d214.org>
2026-04-21 11:18:45 -0500

    changed size for textfeild

--- addrecipe.swift
+++ addrecipe.swift
@@ -6,10 +6,19 @@ struct addrecipe: View {
     @State var cooktime = ""
     @State var sevings  = ""
     var body: some View {
-       TextField("Recipe Name", text: $recipename)
-            .font(.headline)
+        HStack {
+            Text("Recipe Name")
+            TextField("Recipe Name", text: $recipename)
+                 .font(.custom("Baskerville", size: 50))
+            
+        }
+      
+        
         TextField("Oven Temp", text: $oventemp)
+            .font(.custom("Baskerville", size: 50))
         TextField("Cook Time", text: $cooktime)
+            .font(.custom("Baskerville", size: 50))
         TextField("Servings", text: $sevings)
+            .font(.custom("Baskerville", size: 50))
     }
 }
