21139423f8e7aa7c8c307ef3a64d63c064e41dbb
Olivia <olivia.szeszko@gmail.com>
2026-04-21 11:01:57 -0500

    pulling new code. started button

--- ContentView.swift
+++ ContentView.swift
@@ -6,17 +6,26 @@ struct ContentView: View {
         NavigationStack {
             VStack {
                 Text ("Grandmas Cookbook")
-                    .font(.custom("Baskerville", size: 50))
+                    .font(.custom("Baskerville", size: 100))
                 
                 RoundedRectangle(cornerRadius: 20)
-                    .frame(width: 200, height: 100)
+                    .frame(width: 230, height: 130)
+                    .foregroundStyle(Color.brown)
+                    .overlay{
+                        NavigationLink("Add a Recipe") {
+                            LibraryView()
+                        }}.foregroundStyle(.white)
+                    .font(.custom("Baskerville", size: 36))
+                    .padding()
+                
+                RoundedRectangle(cornerRadius: 20)
+                    .frame(width: 230, height: 130)
                     .foregroundStyle(Color.brown)
                     .overlay{
                         NavigationLink("Go to Library") {
                             LibraryView()
                         }}.foregroundStyle(.white)
-                    .font(.custom("Baskerville", size: 30))
-                
+                    .font(.custom("Baskerville", size: 36))
 
             }
         }
