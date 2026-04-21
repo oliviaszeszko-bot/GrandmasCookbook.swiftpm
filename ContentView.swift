c9139ec697fe6635869b3826fa5178da30d96fd8
Zahra Babayeva <zbabayeva9026@stu.d214.org>
2026-04-21 10:31:10 -0500

    added the app title

--- ContentView.swift
+++ ContentView.swift
@@ -3,10 +3,9 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+           Text ("Grandmas Cookbook")
+                .font(.custom("Baskerville", size: 50))
+
         }
     }
 }
