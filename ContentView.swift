c2bbf83f37173ed0bface131ec817136d944bfcd
Olivia <olivia.szeszko@gmail.com>
2026-04-21 11:17:55 -0500

    added a spatula icon

--- ContentView.swift
+++ ContentView.swift
@@ -9,27 +9,35 @@ struct ContentView: View {
                     .resizable()
                     .frame(width: 3300, height: 3000)
                 VStack {
+                    
                     Text ("Grandmas Cookbook")
                         .font(.custom("Baskerville", size: 100))
                         .padding()
-                    RoundedRectangle(cornerRadius: 20)
-                        .frame(width: 230, height: 130)
-                        .foregroundStyle(Color.brown)
-                        .overlay{
-                            NavigationLink("Add a Recipe") {
-                                addrecipe()
-                            }}.foregroundStyle(.white)
-                        .font(.custom("Baskerville", size: 36))
-                        .padding()
-                    
-                    RoundedRectangle(cornerRadius: 20)
-                        .frame(width: 230, height: 130)
-                        .foregroundStyle(Color.brown)
-                        .overlay{
-                            NavigationLink("Go to Library") {
-                                LibraryView()
-                            }}.foregroundStyle(.white)
-                        .font(.custom("Baskerville", size: 36))
+                    HStack{
+                        Image("spatula")
+                            .resizable()
+                            .frame(width: 400, height: 400)
+                        VStack{
+                            RoundedRectangle(cornerRadius: 20)
+                                .frame(width: 230, height: 130)
+                                .foregroundStyle(Color.brown)
+                                .overlay{
+                                    NavigationLink("Add a Recipe") {
+                                        addrecipe()
+                                    }}.foregroundStyle(.white)
+                                .font(.custom("Baskerville", size: 36))
+                                .padding()
+                            
+                            RoundedRectangle(cornerRadius: 20)
+                                .frame(width: 230, height: 130)
+                                .foregroundStyle(Color.brown)
+                                .overlay{
+                                    NavigationLink("Go to Library") {
+                                        LibraryView()
+                                    }}.foregroundStyle(.white)
+                                .font(.custom("Baskerville", size: 36))
+                        }
+                    }
                     
                 }
             }
