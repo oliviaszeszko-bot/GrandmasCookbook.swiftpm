1428196d00ec77ee4689fee2f6015c7c15bde434
Olivia <olivia.szeszko@gmail.com>
2026-04-21 10:50:12 -0500

    made a button

--- ContentView.swift
+++ ContentView.swift
@@ -3,11 +3,22 @@ import SwiftUI
 struct ContentView: View {
     
     var body: some View {
-       
-        VStack {
-           Text ("Grandmas Cookbook")
-                .font(.custom("Baskerville", size: 50))
+        NavigationStack {
+            VStack {
+                Text ("Grandmas Cookbook")
+                    .font(.custom("Baskerville", size: 50))
+                
+                RoundedRectangle(cornerRadius: 20)
+                    .frame(width: 200, height: 100)
+                    .foregroundStyle(Color.brown)
+                    .overlay{
+                        NavigationLink("Go to Library") {
+                            LibraryView()
+                        }}.foregroundStyle(.white)
+                    .font(.custom("Baskerville", size: 30))
+                
 
+            }
         }
     }
 }
