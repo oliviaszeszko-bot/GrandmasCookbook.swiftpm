d764b5d394bcbdcaee54af41a2b1171ea333e00b
Zahra Babayeva <zbabayeva9026@stu.d214.org>
2026-04-21 10:34:34 -0500

    pulling new code

--- ContentView.swift
+++ ContentView.swift
@@ -4,7 +4,7 @@ struct ContentView: View {
     var body: some View {
         VStack {
            Text ("Grandmas Cookbook")
-                .font(.custom("Baskerville", size: 50))
+                .font(.custom("Baskerville", size: 70))
 
         }
     }
