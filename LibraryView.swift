6770df053af070aabfac75d4198fb3f69779744c
Olivia <olivia.szeszko@gmail.com>
2026-04-23 10:59:06 -0500

    added an HStack

--- LibraryView.swift
+++ LibraryView.swift
@@ -53,15 +53,17 @@ struct LibraryView: View {
                     .font(.custom("Baskerville", size: 43))
                     .padding()
             }
-            RoundedRectangle(cornerRadius: 20)
-                .frame(width: 200, height: 200)
-                .foregroundStyle(Color.brown)
-                .overlay{
-                    NavigationLink("Dessert") {
-                        DessertView()
-                    }}.foregroundStyle(.white)
-                .font(.custom("Baskerville", size: 43))
-                .padding()
+            HStack{
+                RoundedRectangle(cornerRadius: 20)
+                    .frame(width: 200, height: 200)
+                    .foregroundStyle(Color.brown)
+                    .overlay{
+                        NavigationLink("Dessert") {
+                            DessertView()
+                        }}.foregroundStyle(.white)
+                    .font(.custom("Baskerville", size: 43))
+                    .padding()
+            }
         }
     }
 }
