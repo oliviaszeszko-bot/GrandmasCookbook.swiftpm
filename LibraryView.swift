9add42f3e46c3adbb0ae877ba165989720d48ed3
Olivia <olivia.szeszko@gmail.com>
2026-04-23 10:59:52 -0500

    moved the button

--- LibraryView.swift
+++ LibraryView.swift
@@ -63,16 +63,17 @@ struct LibraryView: View {
                         }}.foregroundStyle(.white)
                     .font(.custom("Baskerville", size: 43))
                     .padding()
+                RoundedRectangle(cornerRadius: 20)
+                    .frame(width: 200, height: 200)
+                    .foregroundStyle(Color.brown)
+                    .overlay{
+                        NavigationLink("Gluten Free") {
+                            GlutenFreeView()
+                        }}.foregroundStyle(.white)
+                    .font(.custom("Baskerville", size: 43))
+                    .padding()
             }
         }
-        RoundedRectangle(cornerRadius: 20)
-            .frame(width: 200, height: 200)
-            .foregroundStyle(Color.brown)
-            .overlay{
-                NavigationLink("Gluten Free") {
-                    GlutenFreeView()
-                }}.foregroundStyle(.white)
-            .font(.custom("Baskerville", size: 43))
-            .padding()
+        
     }
 }
