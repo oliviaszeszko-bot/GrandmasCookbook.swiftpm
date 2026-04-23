1c095e382c11b7f1a1f2a71b8ef84c60d36eba85
Zahra Babayeva <zbabayeva9026@stu.d214.org>
2026-04-23 11:00:23 -0500

    aded a diebetic section

--- LibraryView.swift
+++ LibraryView.swift
@@ -72,5 +72,14 @@ struct LibraryView: View {
                 }}.foregroundStyle(.white)
             .font(.custom("Baskerville", size: 43))
             .padding()
+        RoundedRectangle(cornerRadius: 20)
+            .frame(width: 200, height: 200)
+            .foregroundStyle(Color.brown)
+            .overlay{
+                NavigationLink("Diabetes") {
+                    DiabeticView()
+                }}.foregroundStyle(.white)
+            .font(.custom("Baskerville", size: 43))
+            .padding()
     }
 }
