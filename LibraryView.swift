762b9bffa1f28a7c8f7e037e2fd6d48ce059f18b
Olivia <olivia.szeszko@gmail.com>
2026-04-23 11:07:23 -0500

    added a background

--- LibraryView.swift
+++ LibraryView.swift
@@ -10,79 +10,84 @@ struct LibraryView: View {
     
     var body: some View {
         
-       Text("Recipe Library")
-            .font(.custom("Baskerville", size: 100))
-            .padding()
-        VStack{
-            HStack{
-                RoundedRectangle(cornerRadius: 20)
-                    .frame(width: 200, height: 200)
-                    .foregroundStyle(Color.brown)
-                    .overlay{
-                        NavigationLink("Breakfast") {
-                            BreakfastView()
-                        }}.foregroundStyle(.white)
-                    .font(.custom("Baskerville", size: 43))
+        ZStack{
+            Image("bookpage")
+                .resizable()
+                .frame(width: 3300, height: 3000)
+            
+            VStack{
+                Text("Recipe Library")
+                    .font(.custom("Baskerville", size: 100))
                     .padding()
-                RoundedRectangle(cornerRadius: 20)
-                    .frame(width: 200, height: 200)
-                    .foregroundStyle(Color.brown)
-                    .overlay{
-                        NavigationLink("Lunch") {
-                            LunchView()
-                        }}.foregroundStyle(.white)
-                    .font(.custom("Baskerville", size: 50))
-                    .padding()
-                RoundedRectangle(cornerRadius: 20)
-                    .frame(width: 200, height: 200)
-                    .foregroundStyle(Color.brown)
-                    .overlay{
-                        NavigationLink("Dinner") {
-                            DinnerView()
+                HStack{
+                    RoundedRectangle(cornerRadius: 20)
+                        .frame(width: 200, height: 200)
+                        .foregroundStyle(Color.brown)
+                        .overlay{
+                            NavigationLink("Breakfast") {
+                                BreakfastView()
+                            }}.foregroundStyle(.white)
+                        .font(.custom("Baskerville", size: 43))
+                        .padding()
+                    RoundedRectangle(cornerRadius: 20)
+                        .frame(width: 200, height: 200)
+                        .foregroundStyle(Color.brown)
+                        .overlay{
+                            NavigationLink("Lunch") {
+                                LunchView()
+                            }}.foregroundStyle(.white)
+                        .font(.custom("Baskerville", size: 50))
+                        .padding()
+                    RoundedRectangle(cornerRadius: 20)
+                        .frame(width: 200, height: 200)
+                        .foregroundStyle(Color.brown)
+                        .overlay{
+                            NavigationLink("Dinner") {
+                                DinnerView()
                             }.foregroundStyle(.white)
-                            .font(.custom("Baskerville", size: 43))
-                            .padding()
-                    }
-                RoundedRectangle(cornerRadius: 20)
-                    .frame(width: 200, height: 200)
-                    .foregroundStyle(Color.brown)
-                    .overlay{
-                        NavigationLink("Snacks") {
-                            SnackView()
-                        }}.foregroundStyle(.white)
-                    .font(.custom("Baskerville", size: 43))
-                    .padding()
-            }
-            HStack{
-                RoundedRectangle(cornerRadius: 20)
-                    .frame(width: 200, height: 200)
-                    .foregroundStyle(Color.brown)
-                    .overlay{
-                        NavigationLink("Dessert") {
-                            DessertView()
-                        }}.foregroundStyle(.white)
-                    .font(.custom("Baskerville", size: 43))
-                    .padding()
-                RoundedRectangle(cornerRadius: 20)
-                    .frame(width: 200, height: 200)
-                    .foregroundStyle(Color.brown)
-                    .overlay{
-                        NavigationLink("Gluten Free") {
-                            GlutenFreeView()
-                        }}.foregroundStyle(.white)
-                    .font(.custom("Baskerville", size: 43))
-                    .padding()
-                RoundedRectangle(cornerRadius: 20)
-                    .frame(width: 200, height: 200)
-                    .foregroundStyle(Color.brown)
-                    .overlay{
-                        NavigationLink("Vegetarian") {
-                            VegetarianView()
-                        }}.foregroundStyle(.white)
-                    .font(.custom("Baskerville", size: 43))
-                    .padding()
+                                .font(.custom("Baskerville", size: 43))
+                                .padding()
+                        }
+                    RoundedRectangle(cornerRadius: 20)
+                        .frame(width: 200, height: 200)
+                        .foregroundStyle(Color.brown)
+                        .overlay{
+                            NavigationLink("Snacks") {
+                                SnackView()
+                            }}.foregroundStyle(.white)
+                        .font(.custom("Baskerville", size: 43))
+                        .padding()
+                }
+                HStack{
+                    RoundedRectangle(cornerRadius: 20)
+                        .frame(width: 200, height: 200)
+                        .foregroundStyle(Color.brown)
+                        .overlay{
+                            NavigationLink("Dessert") {
+                                DessertView()
+                            }}.foregroundStyle(.white)
+                        .font(.custom("Baskerville", size: 43))
+                        .padding()
+                    RoundedRectangle(cornerRadius: 20)
+                        .frame(width: 200, height: 200)
+                        .foregroundStyle(Color.brown)
+                        .overlay{
+                            NavigationLink("Gluten Free") {
+                                GlutenFreeView()
+                            }}.foregroundStyle(.white)
+                        .font(.custom("Baskerville", size: 43))
+                        .padding()
+                    RoundedRectangle(cornerRadius: 20)
+                        .frame(width: 200, height: 200)
+                        .foregroundStyle(Color.brown)
+                        .overlay{
+                            NavigationLink("Vegetarian") {
+                                VegetarianView()
+                            }}.foregroundStyle(.white)
+                        .font(.custom("Baskerville", size: 43))
+                        .padding()
+                }
             }
         }
-        
     }
 }
