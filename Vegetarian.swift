2eb1537fd880c095844330c476b58b5b203742e8
Olivia <olivia.szeszko@gmail.com>
2026-04-23 10:57:30 -0500

    addded a page for vegetraian recipes

---
+++ Vegetarian.swift
@@ -0,0 +1,16 @@
+//
+//  Vegetarian.swift
+//  GrandmasCookbook
+//
+//  Created by Olivia Szeszko on 4/23/26.
+//
+
+import SwiftUI
+
+struct VegetarianView: View {
+    
+    var body: some View {
+       
+
+    }
+}
