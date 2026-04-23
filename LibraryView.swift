6e5bae72810dee43a0f1da204cfa93e0a56e41f6
Olivia <olivia.szeszko@gmail.com>
2026-04-23 11:04:19 -0500

    added a vegetarian button

--- LibraryView.swift
+++ LibraryView.swift
@@ -72,6 +72,15 @@ struct LibraryView: View {
                         }}.foregroundStyle(.white)
                     .font(.custom("Baskerville", size: 43))
                     .padding()
+                RoundedRectangle(cornerRadius: 20)
+                    .frame(width: 200, height: 200)
+                    .foregroundStyle(Color.brown)
+                    .overlay{
+                        NavigationLink("Vegetarian") {
+                            VegetarianView()
+                        }}.foregroundStyle(.white)
+                    .font(.custom("Baskerville", size: 43))
+                    .padding()
             }
         }
         
