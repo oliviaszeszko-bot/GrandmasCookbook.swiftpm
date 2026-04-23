8ec23db7870e1f835eb900c86b7fabddf303a666
Zahra Babayeva <zbabayeva9026@stu.d214.org>
2026-04-23 10:45:03 -0500

    created a button for lunch

--- LibraryView.swift
+++ LibraryView.swift
@@ -9,7 +9,17 @@ import SwiftUI
 struct LibraryView: View {
     
     var body: some View {
-       
-
+        VStack{
+            RoundedRectangle(cornerRadius: 20)
+                .frame(width: 200, height: 200)
+                .foregroundStyle(Color.brown)
+                .overlay{
+                    NavigationLink("Lunch") {
+                        LunchView()
+                    }}.foregroundStyle(.white)
+                .font(.custom("Baskerville", size: 50))
+                .padding()
+            
+        }
     }
 }
