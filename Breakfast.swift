9b890d0083c32de6620b39a358c329e44542e50d
Zahra Babayeva <zbabayeva9026@stu.d214.org>
2026-04-27 10:14:50 -0500

    aded button for 4th recepie

--- Breakfast.swift
+++ Breakfast.swift
@@ -28,6 +28,15 @@ struct BreakfastView: View {
                     .frame(width: 200, height: 200)
                     .foregroundStyle(Color.brown)
                     .overlay{
+                        NavigationLink("Panckkes") {
+                            breakfast4()
+                        }}.foregroundStyle(.white)
+                    .font(.custom("Baskerville", size: 43))
+                    .padding()
+                RoundedRectangle(cornerRadius: 20)
+                    .frame(width: 200, height: 200)
+                    .foregroundStyle(Color.brown)
+                    .overlay{
                         NavigationLink("Shakshuka") {
                             Breakfast1()
                         }}.foregroundStyle(.white)
