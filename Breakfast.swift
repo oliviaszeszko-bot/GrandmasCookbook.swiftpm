ff12cc43af76240c21ff56ac3515b0a63d743c0e
Olivia <olivia.szeszko@gmail.com>
2026-04-27 10:11:12 -0500

    fixed errors

--- Breakfast.swift
+++ Breakfast.swift
@@ -24,18 +24,19 @@ struct BreakfastView: View {
                         }}.foregroundStyle(.white)
                     .font(.custom("Baskerville", size: 43))
                     .padding()
+                RoundedRectangle(cornerRadius: 20)
+                    .frame(width: 200, height: 200)
+                    .foregroundStyle(Color.brown)
+                    .overlay{
+                        NavigationLink("Shakshuka") {
+                            Breakfast1()
+                        }}.foregroundStyle(.white)
+                    .font(.custom("Baskerville", size: 43))
+                    .padding()
                 
             }
         }
-       
-        RoundedRectangle(cornerRadius: 20)
-            .frame(width: 200, height: 200)
-            .foregroundStyle(Color.brown)
-            .overlay{
-                NavigationLink("Shakshuka") {
-                    Breakfast1()
-                }}.foregroundStyle(.white)
-            .font(.custom("Baskerville", size: 43))
-            .padding()
+        
+        
     }
-
+}
