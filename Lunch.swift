5e3a760de867388222f9ebc95059e67676576f56
Olivia <olivia.szeszko@gmail.com>
2026-04-27 10:43:20 -0500

    added a button for recipe lunch 2

--- Lunch.swift
+++ Lunch.swift
@@ -10,15 +10,25 @@ import SwiftUI
 struct LunchView: View {
     
     var body: some View {
-       
-        RoundedRectangle(cornerRadius: 20)
-            .frame(width: 200, height: 200)
-            .foregroundStyle(Color.brown)
-            .overlay{
-                NavigationLink("Club Sandwich") {
-                    lunch1()
-                }}.foregroundStyle(.white)
-            .font(.custom("Baskerville", size: 43))
-            .padding()
+        HStack{
+            RoundedRectangle(cornerRadius: 20)
+                .frame(width: 200, height: 200)
+                .foregroundStyle(Color.brown)
+                .overlay{
+                    NavigationLink("Club Sandwich") {
+                        lunch1()
+                    }}.foregroundStyle(.white)
+                .font(.custom("Baskerville", size: 43))
+                .padding()
+            RoundedRectangle(cornerRadius: 20)
+                .frame(width: 200, height: 200)
+                .foregroundStyle(Color.brown)
+                .overlay{
+                    NavigationLink("Chicken Caeser Salad") {
+                        lunch2()
+                    }}.foregroundStyle(.white)
+                .font(.custom("Baskerville", size: 43))
+                .padding()
+        }
     }
 }
