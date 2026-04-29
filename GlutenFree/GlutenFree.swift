272f94e9b21599ef3f3f8922b51d774249eb9e31
Olivia <olivia.szeszko@gmail.com>
2026-04-29 10:50:50 -0500

    added a button for GF 3

--- GlutenFree/GlutenFree.swift
+++ GlutenFree/GlutenFree.swift
@@ -25,7 +25,16 @@ struct GlutenFreeView: View {
                 .foregroundStyle(Color.brown)
                 .overlay{
                     NavigationLink("Shrimp Tacos") {
-                        GF1()
+                        GF2()
+                    }}.foregroundStyle(.white)
+                .font(.custom("Baskerville", size: 43))
+                .padding()
+            RoundedRectangle(cornerRadius: 20)
+                .frame(width: 200, height: 200)
+                .foregroundStyle(Color.brown)
+                .overlay{
+                    NavigationLink("Corn Tortillas") {
+                        GF3()
                     }}.foregroundStyle(.white)
                 .font(.custom("Baskerville", size: 43))
                 .padding()
