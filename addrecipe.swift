4691b034d2dbd1765f22d734efa99a630c8f310a
Olivia <olivia.szeszko@gmail.com>
2026-04-29 11:05:06 -0500

    added modifiers to add recipe page

--- addrecipe.swift
+++ addrecipe.swift
@@ -6,41 +6,49 @@ struct addrecipe: View {
     @State var cooktime = ""
     @State var sevings  = ""
     var body: some View {
-//        ZStack{
-//            Image("bookpage")
-//                .resizable()
-//                .frame(width: 3300, height: 3000)
-            HStack{
-                Text("Recipe Name")
-                    .font(.custom("Baskerville", size: 35))
-                TextField("", text: $recipename)
-                    .font(.title)
-                    .textFieldStyle(.roundedBorder)
-            }
-            HStack{
-                Text("Oven Temp")
-                    .font(.custom("Baskerville", size: 35))
-                TextField("", text: $oventemp)
-                    .font(.title)
-                    .textFieldStyle(.roundedBorder)
-            }
-            HStack{
-                Text("Cook Time")
-                    .font(.custom("Baskerville", size: 35))
-                TextField("", text: $cooktime)
-                    .font(.title)
-                    .textFieldStyle(.roundedBorder)
-            }
-            HStack{
-                Text("Servings")
-                    .font(.custom("Baskerville", size: 35))
-                TextField("", text: $sevings)
-                    .font(.title)
-                    .textFieldStyle(.roundedBorder)
+        ZStack{
+            Image("bookpage")
+                .resizable()
+                .frame(width: 3300, height: 3000)
+            VStack{
+                HStack{
+                    Text("Recipe Name")
+                        .font(.custom("Baskerville", size: 65))
+                    TextField("", text: $recipename)
+                        .font(.title)
+                        .textFieldStyle(.roundedBorder)
+                        .frame(width: 600, height: 40)
+                        .border(Color.black, width: 3)
+                }
+                HStack{
+                    Text("Oven Temp")
+                        .font(.custom("Baskerville", size: 65))
+                    TextField("", text: $oventemp)
+                        .font(.title)
+                        .textFieldStyle(.roundedBorder)
+                        .frame(width: 600, height: 40)
+                        .border(Color.black, width: 3)
+                }
+                HStack{
+                    Text("Cook Time")
+                        .font(.custom("Baskerville", size: 65))
+                    TextField("", text: $cooktime)
+                        .font(.title)
+                        .textFieldStyle(.roundedBorder)
+                        .frame(width: 600, height: 40)
+                        .border(Color.black, width: 3)
+                }
+                HStack{
+                    Text("Servings")
+                        .font(.custom("Baskerville", size: 72))
+                    TextField("", text: $sevings)
+                        .font(.title)
+                        .textFieldStyle(.roundedBorder)
+                        .frame(width: 600, height: 40)
+                        .border(Color.black, width: 3)
+                }
             }
         }
-//        #Preview(traits: .landscapeLeft) {
-//            ContentView()
-        }
-    
+    }
+}
 
