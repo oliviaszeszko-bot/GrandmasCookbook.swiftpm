bdcc2fc0a153be3707dba2dd9dc3b3a2a5edd711
Olivia <olivia.szeszko@gmail.com>
2026-04-29 10:28:04 -0500

    added a vegie recipe button 2

--- Vegiterian /Vegetarian.swift
+++ Vegiterian /Vegetarian.swift
@@ -16,7 +16,16 @@ struct VegetarianView: View {
                 .foregroundStyle(Color.brown)
                 .overlay{
                     NavigationLink("Tofu and Vegetable Wraps") {
-                        dinner1()
+                        Vegetarian1()
+                    }}.foregroundStyle(.white)
+                .font(.custom("Baskerville", size: 57))
+                .padding()
+            RoundedRectangle(cornerRadius: 20)
+                .frame(width: 200, height: 200)
+                .foregroundStyle(Color.brown)
+                .overlay{
+                    NavigationLink("Red Lentil Soup") {
+                        Vegetarian2()
                     }}.foregroundStyle(.white)
                 .font(.custom("Baskerville", size: 57))
                 .padding()
