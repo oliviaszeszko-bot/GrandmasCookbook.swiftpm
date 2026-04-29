c4f9af6cb8efa8c0ac23319f8c096702b050261c
Olivia <olivia.szeszko@gmail.com>
2026-04-29 10:54:17 -0500

    added a struct

--- GlutenFree/GlutenFree.swift
+++ GlutenFree/GlutenFree.swift
@@ -17,43 +17,44 @@ struct GlutenFreeView: View {
             Text("Gluten Free")
                 .font(.custom("Baskerville", size: 145))
                 .padding()
-        HStack{
-            RoundedRectangle(cornerRadius: 20)
-                .frame(width: 200, height: 200)
-                .foregroundStyle(Color.brown)
-                .overlay{
-                    NavigationLink("Califlower and Bean Paella") {
-                        GF1()
-                    }}.foregroundStyle(.white)
-                .font(.custom("Baskerville", size: 43))
-                .padding()
-            RoundedRectangle(cornerRadius: 20)
-                .frame(width: 200, height: 200)
-                .foregroundStyle(Color.brown)
-                .overlay{
-                    NavigationLink("Shrimp Tacos") {
-                        GF2()
-                    }}.foregroundStyle(.white)
-                .font(.custom("Baskerville", size: 43))
-                .padding()
-            RoundedRectangle(cornerRadius: 20)
-                .frame(width: 200, height: 200)
-                .foregroundStyle(Color.brown)
-                .overlay{
-                    NavigationLink("Corn Tortillas") {
-                        GF3()
-                    }}.foregroundStyle(.white)
-                .font(.custom("Baskerville", size: 43))
-                .padding()
-            RoundedRectangle(cornerRadius: 20)
-                .frame(width: 200, height: 200)
-                .foregroundStyle(Color.brown)
-                .overlay{
-                    NavigationLink("Roasted Lemon Salmon") {
-                        GF4()
-                    }}.foregroundStyle(.white)
-                .font(.custom("Baskerville", size: 43))
-                .padding()
+            HStack{
+                RoundedRectangle(cornerRadius: 20)
+                    .frame(width: 200, height: 200)
+                    .foregroundStyle(Color.brown)
+                    .overlay{
+                        NavigationLink("Califlower and Bean Paella") {
+                            GF1()
+                        }}.foregroundStyle(.white)
+                    .font(.custom("Baskerville", size: 43))
+                    .padding()
+                RoundedRectangle(cornerRadius: 20)
+                    .frame(width: 200, height: 200)
+                    .foregroundStyle(Color.brown)
+                    .overlay{
+                        NavigationLink("Shrimp Tacos") {
+                            GF2()
+                        }}.foregroundStyle(.white)
+                    .font(.custom("Baskerville", size: 43))
+                    .padding()
+                RoundedRectangle(cornerRadius: 20)
+                    .frame(width: 200, height: 200)
+                    .foregroundStyle(Color.brown)
+                    .overlay{
+                        NavigationLink("Corn Tortillas") {
+                            GF3()
+                        }}.foregroundStyle(.white)
+                    .font(.custom("Baskerville", size: 43))
+                    .padding()
+                RoundedRectangle(cornerRadius: 20)
+                    .frame(width: 200, height: 200)
+                    .foregroundStyle(Color.brown)
+                    .overlay{
+                        NavigationLink("Roasted Lemon Salmon") {
+                            GF4()
+                        }}.foregroundStyle(.white)
+                    .font(.custom("Baskerville", size: 43))
+                    .padding()
+            }
         }
     }
 }
