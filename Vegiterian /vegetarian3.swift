590a0e8477a15bc7af7d64dac894694a4d1af1b4
Olivia <olivia.szeszko@gmail.com>
2026-04-29 10:17:08 -0500

    added a new page for vegie 3

---
+++ Vegiterian /vegetarian3.swift
@@ -0,0 +1,17 @@
+//
+//  vegetarian3.swift
+//  GrandmasCookbook
+//
+//  Created by Olivia Szeszko on 4/29/26.
+//
+
+
+import SwiftUI
+
+struct Vegetarian3: View {
+    
+    var body: some View {
+       
+
+    }
+}
