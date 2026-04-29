12a37894eb8a23eb4a364da757129c86def2167d
Olivia <olivia.szeszko@gmail.com>
2026-04-29 10:53:49 -0500

    added a button for Gf 4

--- GlutenFree/GlutenFree.swift
+++ GlutenFree/GlutenFree.swift
@@ -38,6 +38,15 @@ struct GlutenFreeView: View {
                     }}.foregroundStyle(.white)
                 .font(.custom("Baskerville", size: 43))
                 .padding()
+            RoundedRectangle(cornerRadius: 20)
+                .frame(width: 200, height: 200)
+                .foregroundStyle(Color.brown)
+                .overlay{
+                    NavigationLink("Roasted Lemon Salmon") {
+                        GF4()
+                    }}.foregroundStyle(.white)
+                .font(.custom("Baskerville", size: 43))
+                .padding()
         }
     }
 }
