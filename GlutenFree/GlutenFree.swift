6f7b82d75dde982bd42ec901768a869841845ad8
Olivia <olivia.szeszko@gmail.com>
2026-04-29 10:55:46 -0500

    added a ZStack

--- GlutenFree/GlutenFree.swift
+++ GlutenFree/GlutenFree.swift
@@ -10,50 +10,52 @@ import SwiftUI
 struct GlutenFreeView: View {
     
     var body: some View {
-        Image("bookpage")
-            .resizable()
-            .frame(width: 3300, height: 3000)
-        VStack{
-            Text("Gluten Free")
-                .font(.custom("Baskerville", size: 145))
-                .padding()
-            HStack{
-                RoundedRectangle(cornerRadius: 20)
-                    .frame(width: 200, height: 200)
-                    .foregroundStyle(Color.brown)
-                    .overlay{
-                        NavigationLink("Califlower and Bean Paella") {
-                            GF1()
-                        }}.foregroundStyle(.white)
-                    .font(.custom("Baskerville", size: 43))
+        ZStack{
+            Image("bookpage")
+                .resizable()
+                .frame(width: 3300, height: 3000)
+            VStack{
+                Text("Gluten Free")
+                    .font(.custom("Baskerville", size: 145))
                     .padding()
-                RoundedRectangle(cornerRadius: 20)
-                    .frame(width: 200, height: 200)
-                    .foregroundStyle(Color.brown)
-                    .overlay{
-                        NavigationLink("Shrimp Tacos") {
-                            GF2()
-                        }}.foregroundStyle(.white)
-                    .font(.custom("Baskerville", size: 43))
-                    .padding()
-                RoundedRectangle(cornerRadius: 20)
-                    .frame(width: 200, height: 200)
-                    .foregroundStyle(Color.brown)
-                    .overlay{
-                        NavigationLink("Corn Tortillas") {
-                            GF3()
-                        }}.foregroundStyle(.white)
-                    .font(.custom("Baskerville", size: 43))
-                    .padding()
-                RoundedRectangle(cornerRadius: 20)
-                    .frame(width: 200, height: 200)
-                    .foregroundStyle(Color.brown)
-                    .overlay{
-                        NavigationLink("Roasted Lemon Salmon") {
-                            GF4()
-                        }}.foregroundStyle(.white)
-                    .font(.custom("Baskerville", size: 43))
-                    .padding()
+                HStack{
+                    RoundedRectangle(cornerRadius: 20)
+                        .frame(width: 200, height: 200)
+                        .foregroundStyle(Color.brown)
+                        .overlay{
+                            NavigationLink("Califlower and Bean Paella") {
+                                GF1()
+                            }}.foregroundStyle(.white)
+                        .font(.custom("Baskerville", size: 43))
+                        .padding()
+                    RoundedRectangle(cornerRadius: 20)
+                        .frame(width: 200, height: 200)
+                        .foregroundStyle(Color.brown)
+                        .overlay{
+                            NavigationLink("Shrimp Tacos") {
+                                GF2()
+                            }}.foregroundStyle(.white)
+                        .font(.custom("Baskerville", size: 43))
+                        .padding()
+                    RoundedRectangle(cornerRadius: 20)
+                        .frame(width: 200, height: 200)
+                        .foregroundStyle(Color.brown)
+                        .overlay{
+                            NavigationLink("Corn Tortillas") {
+                                GF3()
+                            }}.foregroundStyle(.white)
+                        .font(.custom("Baskerville", size: 43))
+                        .padding()
+                    RoundedRectangle(cornerRadius: 20)
+                        .frame(width: 200, height: 200)
+                        .foregroundStyle(Color.brown)
+                        .overlay{
+                            NavigationLink("Roasted Lemon Salmon") {
+                                GF4()
+                            }}.foregroundStyle(.white)
+                        .font(.custom("Baskerville", size: 43))
+                        .padding()
+                }
             }
         }
     }
