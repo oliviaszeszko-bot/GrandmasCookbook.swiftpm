520bd23b4e0eb6944e8e896179b917fc9652c31e
Zahra Babayeva <zbabayeva9026@stu.d214.org>
2026-04-29 10:33:13 -0500

    aded title and backgroung to diebetic

---
+++ Diabetic/sugar1.swift
@@ -0,0 +1,7 @@
+//
+//  sugar1.swift
+//  GrandmasCookbook
+//
+//  Created by Zahra N. Babayeva on 4/29/26.
+//
+
---
+++ Diabetic/sugar2.swift
@@ -0,0 +1,7 @@
+//
+//  sugar2.swift
+//  GrandmasCookbook
+//
+//  Created by Zahra N. Babayeva on 4/29/26.
+//
+
