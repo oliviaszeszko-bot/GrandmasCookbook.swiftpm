5285f41c9428bb4e945d2b55281ae8f03746477a
Olivia <olivia.szeszko@gmail.com>
2026-05-01 10:04:51 -0500

    added a title for igredients page

--- addRecipe/addRecipe2.swift
+++ addRecipe/addRecipe2.swift
@@ -10,7 +10,9 @@ import SwiftUI
 struct addRecipe2: View {
     
     var body: some View {
-       
+        Text("Ingredients")
+            .font(.custom("Baskerville", size: 145))
+            .padding()
 
     }
 }
