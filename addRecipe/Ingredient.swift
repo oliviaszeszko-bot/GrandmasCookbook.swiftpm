ac4cba1e6334721f750e947d4140bb9f070b2523
Olivia <olivia.szeszko@gmail.com>
2026-05-01 10:09:57 -0500

    made an indregient item

---
+++ addRecipe/Ingredient.swift
@@ -0,0 +1,11 @@
+//
+//  Ingredient.swift
+//  GrandmasCookbook
+//
+//  Created by Olivia Szeszko on 5/1/26.
+//
+
+struct Ingreident : Hashable {
+    var ingredient: String
+    var quantity: Int?
+}
