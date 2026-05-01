c3499e8766869967243ff84b520f443421f23689
Zahra Babayeva <zbabayeva9026@stu.d214.org>
2026-05-01 11:11:00 -0500

    aded split view to breackfast recepie 1 and 2

--- Breackfast/breakfast1.swift
+++ Breackfast/breakfast1.swift
@@ -1,16 +1,49 @@
-//
-//  breakfast1.swift
-//  GrandmasCookbook
-//
-//  Created by Olivia Szeszko on 4/27/26.
-//
 
 import SwiftUI
 
 struct Breakfast1: View {
     
-    var body: some View {
-       
-
+    var body: some View{
+        HStack (spacing: 305){
+            VStack(alignment: .leading) {
+                Button(action: {
+                    print("Back tapped")
+                }) {
+                    Image (systemName: "arrow.left")
+                        .font(.title)
+                        .padding()
+                }
+                Text("Waffels")
+                    .font(.custom("Georgia", size: 58))
+                    .padding(.horizontal)
+                
+                Divider()
+                VStack {
+                    Text("recepei")
+                        .font(.custom("Georgia", size: 30))
+                    
+                }
+                .padding()
+                
+                Spacer()
+            }
+            .frame(maxWidth: .infinity)
+            
+            Divider()
+                .frame(width: 2)
+                .background(Color.gray)
+            VStack {
+                Text("Ingredients")
+                    .font(.custom("Georgia", size: 43))
+            }
+            Spacer()
+        }
+        .frame(maxWidth: .infinity)
     }
+//        .background(Color.white)
+//        .ignoresSafeArea()
+   }
+#Preview(traits: .landscapeLeft) {
+    ContentView()
 }
+
