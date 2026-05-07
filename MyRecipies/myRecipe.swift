483a25de86158e9ef7fadb1d861e6d9073326061
Olivia <olivia.szeszko@gmail.com>
2026-05-07 10:03:08 -0500

    added a new page for my recipes

---
+++ MyRecipies/myRecipe.swift
@@ -0,0 +1,15 @@
+//
+//  RecipeView.swift
+//  GrandmasCookbook
+//
+//  Created by Olivia Szeszko on 5/7/26.
+//
+
+import SwiftUI
+
+struct myRecipe: View {
+    
+    var body: some View {
+        
+    }
+}
