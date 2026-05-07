97b1e81f47d680c3185be9e1abb7878acfccc7c8
Olivia <olivia.szeszko@gmail.com>
2026-05-07 10:19:50 -0500

    added a title for my recipe page

--- MyRecipies/myRecipe.swift
+++ MyRecipies/myRecipe.swift
@@ -10,43 +10,48 @@ import SwiftUI
 struct myRecipe: View {
     
     var body: some View {
-        HStack{
-            RoundedRectangle(cornerRadius: 20)
-                .frame(width: 250, height: 250)
-                .foregroundStyle(Color.brown)
-                .overlay{
-                    NavigationLink("My Recipe 1") {
-                        myRecipe1()
-                    }}.foregroundStyle(.white)
-                .font(.custom("Baskerville", size: 43))
+        VStack{
+            Text("My Recipes")
+                .font(.custom("Baskerville", size: 190))
                 .padding()
-            RoundedRectangle(cornerRadius: 20)
-                .frame(width: 250, height: 250)
-                .foregroundStyle(Color.brown)
-                .overlay{
-                    NavigationLink("My Recipe 2") {
-                        myRecipe2()
-                    }}.foregroundStyle(.white)
-                .font(.custom("Baskerville", size: 43))
-                .padding()
-            RoundedRectangle(cornerRadius: 20)
-                .frame(width: 250, height: 250)
-                .foregroundStyle(Color.brown)
-                .overlay{
-                    NavigationLink("My Recipe 3") {
-                        myRecipe3()
-                    }}.foregroundStyle(.white)
-                .font(.custom("Baskerville", size: 43))
-                .padding()
-            RoundedRectangle(cornerRadius: 20)
-                .frame(width: 250, height: 250)
-                .foregroundStyle(Color.brown)
-                .overlay{
-                    NavigationLink("My Recipe 4") {
-                        myRecipe4()
-                    }}.foregroundStyle(.white)
-                .font(.custom("Baskerville", size: 43))
-                .padding()
+            HStack{
+                RoundedRectangle(cornerRadius: 20)
+                    .frame(width: 250, height: 250)
+                    .foregroundStyle(Color.brown)
+                    .overlay{
+                        NavigationLink("My Recipe 1") {
+                            myRecipe1()
+                        }}.foregroundStyle(.white)
+                    .font(.custom("Baskerville", size: 43))
+                    .padding()
+                RoundedRectangle(cornerRadius: 20)
+                    .frame(width: 250, height: 250)
+                    .foregroundStyle(Color.brown)
+                    .overlay{
+                        NavigationLink("My Recipe 2") {
+                            myRecipe2()
+                        }}.foregroundStyle(.white)
+                    .font(.custom("Baskerville", size: 43))
+                    .padding()
+                RoundedRectangle(cornerRadius: 20)
+                    .frame(width: 250, height: 250)
+                    .foregroundStyle(Color.brown)
+                    .overlay{
+                        NavigationLink("My Recipe 3") {
+                            myRecipe3()
+                        }}.foregroundStyle(.white)
+                    .font(.custom("Baskerville", size: 43))
+                    .padding()
+                RoundedRectangle(cornerRadius: 20)
+                    .frame(width: 250, height: 250)
+                    .foregroundStyle(Color.brown)
+                    .overlay{
+                        NavigationLink("My Recipe 4") {
+                            myRecipe4()
+                        }}.foregroundStyle(.white)
+                    .font(.custom("Baskerville", size: 43))
+                    .padding()
+            }
         }
     }
 }
