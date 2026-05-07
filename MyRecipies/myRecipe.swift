8b3e3c3961ca58eabc81e7b6db6df25093e046c9
Olivia <olivia.szeszko@gmail.com>
2026-05-07 10:15:44 -0500

    added a button for my recipe 1

--- MyRecipies/myRecipe.swift
+++ MyRecipies/myRecipe.swift
@@ -10,6 +10,16 @@ import SwiftUI
 struct myRecipe: View {
     
     var body: some View {
-        
+        HStack{
+            RoundedRectangle(cornerRadius: 20)
+                .frame(width: 250, height: 250)
+                .foregroundStyle(Color.brown)
+                .overlay{
+                    NavigationLink("My Recipe 1") {
+                        myRecipe1()
+                    }}.foregroundStyle(.white)
+                .font(.custom("Baskerville", size: 43))
+                .padding()
+        }
     }
 }
