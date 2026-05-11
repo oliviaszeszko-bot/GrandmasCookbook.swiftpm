23ca52773bbf906084513364dfd119a858d227c1
Olivia <olivia.szeszko@gmail.com>
2026-05-11 11:11:24 -0500

    added ingredient details to all dessert recipes

--- Deserts/desert2.swift
+++ Deserts/desert2.swift
@@ -9,8 +9,18 @@ import SwiftUI
 
 struct desert2: View {
     
+    @State var buttonColor: Color = .gray
+    @State var buttonColor1: Color = .gray
+    @State var buttonColor2: Color = .gray
+    @State var buttonColor3: Color = .gray
+    @State var buttonColor4: Color = .gray
+    @State var buttonColor5: Color = .gray
+    @State var buttonColor6: Color = .gray
+    @State var buttonColor7: Color = .gray
+    @State var buttonColor8: Color = .gray
     var body: some View {
-        HStack (spacing: 305){
+        
+        HStack {
             VStack(alignment: .leading) {
                 Button(action: {
                     print("Back tapped")
@@ -19,37 +29,175 @@ struct desert2: View {
                         .font(.title)
                         .padding()
                 }
-                Text("Waffels")
+                Text("Bannana Muffins")
                     .font(.custom("Georgia", size: 58))
                     .padding(.horizontal)
                 
                 Divider()
-                VStack {
-                    Text("recepei")
-                        .font(.custom("Georgia", size: 30))
-                    
+                    .frame(width: 570)
+                ScrollView{
+                    VStack(alignment: .leading) {
+                        Text("Ingredients")
+                            .font(.custom("Georgia", size: 50))
+                        Text("")
+                        HStack{
+                            Button {
+                                if buttonColor == .gray {
+                                    self.buttonColor = .green
+                                } else if buttonColor == .green {
+                                    self.buttonColor = .gray
+                                }
+                            } label: {
+                                Image(systemName: "checkmark.circle")
+                                    .font(.largeTitle)
+                                    .foregroundStyle(buttonColor)
+                            }
+                            Text("1 1/2 cups all-purpose flour")
+                                .font(.custom("Arial", size: 25))
+                        }
+                        HStack{
+                            Button {
+                                if buttonColor1 == .gray {
+                                    self.buttonColor1 = .green
+                                } else if buttonColor1 == .green {
+                                    self.buttonColor1 = .gray
+                                }
+                            } label: {
+                                Image(systemName: "checkmark.circle")
+                                    .font(.largeTitle)
+                                    .foregroundStyle(buttonColor1)
+                            }
+                            Text("1 teaspoon baking powder")
+                                .font(.custom("Arial", size: 25))
+                        }
+                        HStack{
+                            Button {
+                                if buttonColor2 == .gray {
+                                    self.buttonColor2 = .green
+                                } else if buttonColor2 == .green {
+                                    self.buttonColor2 = .gray
+                                }
+                            } label: {
+                                Image(systemName: "checkmark.circle")
+                                    .font(.largeTitle)
+                                    .foregroundStyle(buttonColor2)
+                            }
+                            Text("1 teaspoon baking soda")
+                                .font(.custom("Arial", size: 25))
+                        }
+                        HStack{
+                            Button {
+                                if buttonColor3 == .gray {
+                                    self.buttonColor3 = .green
+                                } else if buttonColor3 == .green {
+                                    self.buttonColor3 = .gray
+                                }
+                            } label: {
+                                Image(systemName: "checkmark.circle")
+                                    .font(.largeTitle)
+                                    .foregroundStyle(buttonColor3)
+                            }
+                            Text("3-4 ripe bannanas")
+                                .font(.custom("Arial", size: 25))
+                        }
+                        HStack{
+                            Button {
+                                if buttonColor4 == .gray {
+                                    self.buttonColor4 = .green
+                                } else if buttonColor4 == .green {
+                                    self.buttonColor4 = .gray
+                                }
+                            } label: {
+                                Image(systemName: "checkmark.circle")
+                                    .font(.largeTitle)
+                                    .foregroundStyle(buttonColor4)
+                            }
+                            Text("6 tablespoons unsalted butter, melted")
+                                .font(.custom("Arial", size: 25))
+                        }
+                        HStack{
+                            Button {
+                                if buttonColor5 == .gray {
+                                    self.buttonColor5 = .green
+                                } else if buttonColor5 == .green {
+                                    self.buttonColor5 = .gray
+                                }
+                            } label: {
+                                Image(systemName: "checkmark.circle")
+                                    .font(.largeTitle)
+                                    .foregroundStyle(buttonColor5)
+                            }
+                            Text("1 teaspoon vanilla extract")
+                                .font(.custom("Arial", size: 25))
+                        }
+                        HStack{
+                            Button {
+                                if buttonColor6 == .gray {
+                                    self.buttonColor6 = .green
+                                } else if buttonColor6 == .green {
+                                    self.buttonColor6 = .gray
+                                }
+                            } label: {
+                                Image(systemName: "checkmark.circle")
+                                    .font(.largeTitle)
+                                    .foregroundStyle(buttonColor6)
+                            }
+                            Text("2/3 cup packed brown sugar")
+                                .font(.custom("Arial", size: 25))
+                        }
+                        HStack{
+                            Button {
+                                if buttonColor7 == .gray {
+                                    self.buttonColor7 = .green
+                                } else if buttonColor7 == .green {
+                                    self.buttonColor7 = .gray
+                                }
+                            } label: {
+                                Image(systemName: "checkmark.circle")
+                                    .font(.largeTitle)
+                                    .foregroundStyle(buttonColor7)
+                            }
+                            Text("1 large egg")
+                                .font(.custom("Arial", size: 25))
+                        }
+                        HStack{
+                            Button {
+                                if buttonColor8 == .gray {
+                                    self.buttonColor8 = .green
+                                } else if buttonColor8 == .green {
+                                    self.buttonColor8 = .gray
+                                }
+                            } label: {
+                                Image(systemName: "checkmark.circle")
+                                    .font(.largeTitle)
+                                    .foregroundStyle(buttonColor8)
+                            }
+                            Text("2 talblespoons milk")
+                                .font(.custom("Arial", size: 25))
+                        }
+                    }
                 }
                 .padding()
-                
                 Spacer()
             }
-            .frame(maxWidth: .infinity)
             
             Divider()
-                .frame(width: 2)
+                .frame(width: 3)
                 .background(Color.gray)
             VStack {
-                Text("Ingredients")
+                Text("Instructions")
                     .font(.custom("Georgia", size: 43))
+                
             }
             Spacer()
         }
         .frame(maxWidth: .infinity)
     }
-   }
-#Preview(traits: .landscapeLeft) {
-    ContentView()
 }
+#Preview(traits: .landscapeLeft) {
+    desert2()
+}
+
 
 
 
