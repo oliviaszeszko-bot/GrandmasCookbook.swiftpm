51b4636702ffb41ad735b9ff640730cfaa37962c
Olivia <olivia.szeszko@gmail.com>
2026-05-15 10:22:17 -0500

    added the instructions functionality to the add recipe sequence

---
+++ addRecipe/Instruction.swift
@@ -0,0 +1,10 @@
+//
+//  Instruction.swift
+//  GrandmasCookbook
+//
+//  Created by Olivia Szeszko on 5/15/26.
+//
+
+struct Instruction : Hashable {
+    var step: String
+}
--- addRecipe/addRecipe3.swift
+++ addRecipe/addRecipe3.swift
@@ -8,9 +8,68 @@
 import SwiftUI
 
 struct addRecipe3: View {
-    
+    @State var recipe2: [Instruction] = []
     var body: some View {
-       
-
+        ZStack{
+            Image("bookpage")
+                .resizable()
+                .frame(maxWidth: .infinity, maxHeight: .infinity)
+                
+            VStack{
+                Text("Instructions")
+                    .font(.custom("Baskerville", size: 125))
+                    .padding()
+                AddStepView(recipe2: $recipe2)
+                List() {
+                    ForEach(recipe2, id: \.self) { currentStep in
+                        MyInstructionView(currentStep: currentStep)
+                    }
+                }
+                .scrollContentBackground(.hidden)
+                HStack{
+                    RoundedRectangle(cornerRadius: 20)
+                        .frame(width: 200, height: 129)
+                        .foregroundStyle(Color.brown)
+                        .overlay{
+                            NavigationLink("Save to my Recipe 1") {
+                                myRecipe()
+                            }}.foregroundStyle(.white)
+                        .font(.custom("Baskerville", size: 39))
+                        .padding()
+                    RoundedRectangle(cornerRadius: 20)
+                        .frame(width: 200, height: 129)
+                        .foregroundStyle(Color.brown)
+                        .overlay{
+                            NavigationLink("Save to my Recipe 2") {
+                                myRecipe()
+                            }}.foregroundStyle(.white)
+                        .font(.custom("Baskerville", size: 39))
+                        .padding()
+                    RoundedRectangle(cornerRadius: 20)
+                        .frame(width: 200, height: 129)
+                        .foregroundStyle(Color.brown)
+                        .overlay{
+                            NavigationLink("Save to my Recipe 3") {
+                                myRecipe()
+                            }}.foregroundStyle(.white)
+                        .font(.custom("Baskerville", size: 39))
+                        .padding()
+                        .onTapGesture {
+                            
+                        }
+                    RoundedRectangle(cornerRadius: 20)
+                        .frame(width: 200, height: 129)
+                        .foregroundStyle(Color.brown)
+                        .overlay{
+                            NavigationLink("Save to my Recipe 4") {
+                                myRecipe()
+                            }}.foregroundStyle(.white)
+                        .font(.custom("Baskerville", size: 39))
+                        .padding()
+                }
+            }
+        }.ignoresSafeArea()
     }
 }
+
+
